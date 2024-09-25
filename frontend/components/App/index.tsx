6c9f06fe531221cda3193836a576ddf0366165f7
bobeu <bobmatea27@gmail.com>
2024-09-25 11:22:29 +0100

    Updated_3

--- frontend/components/App/index.tsx
+++ frontend/components/App/index.tsx
@@ -40,7 +40,7 @@ export default function App(props: AppProps) {
     // if(displayAppScreen && isConnected) {
     //   navigate('/dashboard', {replace: true});
     // }
-  }, [displayAppScreen, isConnected, modalOpen, navigate]);
+  }, [displayAppScreen, isConnected, modalOpen ]);
 
   const DrawerHeader = styled('div')(({ theme }) => ({
     display: 'flex',
